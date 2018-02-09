741ea059435029f38ca9a96d78b3a64547782862
GiantsLoveDeathMetal <sebastien@cytora.com>
2018-02-09 01:04:15 +0000

    Save lookup locally

--- define/cli.py
+++ define/cli.py
@@ -4,6 +4,12 @@ import json
 
 import requests
 
+from define.local_lookup import word_exists
+from define.local_lookup import load_words
+from define.local_lookup import get_word
+from define.local_lookup import save_file
+from define.local_lookup import save_words
+
 
 LANGUAGE = 'en'
 URL = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/{language}/{word}'
@@ -16,14 +22,26 @@ HEADERS = {
     'app_key': APP_KEY,
 }
 
+def format_response(response):
+    print(response)
+
 
 def main():
     word = sys.argv[1]
-    url = URL.format(language=LANGUAGE, word=word)
 
-    response = requests.get(url, headers=HEADERS)
-    print("status: {}".format(response.status_code))
-    # print("text: {}".format(response.text))
-    word_definition =  json.loads(response.text)
+    all_words = load_words()
+    if word_exists(word, all_words):
+        word_path = all_words[word]
+        response = get_word(word_path)
+    else:
+        url = URL.format(language=LANGUAGE, word=word)
+        response = requests.get(url, headers=HEADERS)
+        if response.status_code == 200:
+            new_file = '/.dictionary/words/{}.pkl'.format(word)
+            response = json.loads(response.text)
+            all_words[word] = new_file
+            save_words(all_words)
+            save_file(new_file, response)
 
-    print(word_definition['metadata'])
+    format_response(response)
+
